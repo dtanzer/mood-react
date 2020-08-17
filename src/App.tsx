239b3e80109e0e45713d54b7c4161fabfbc913ce
David Tanzer <business@davidtanzer.net>
2020-08-17 06:21:09 +0200

    Add Outer and Inner comp for showing data flow
    There are now two very simple function components, <Outer/> and
    <Inner/>, to demonstrate how React's one-way data flow works -
    and how one can implement the "way back".

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Hello } from './Hello';
 import './App.css';
 import { ToolbarButton, ToolbarButtonClass, Toolbar } from './ToolbarButton';
 
+function Inner(props: any) {
+  return <button onClick={e => props.onInnerClicked() }>{props.someValue}</button>
+}
+function Outer() {
+  const [val, setVal] = useState('concrete value')
+  return <Inner onInnerClicked={() => setVal('clicked') } someValue={val} />
+}
 function App() {
   return (
     <div className="App">
@@ -12,6 +19,8 @@ function App() {
           <ToolbarButton text="func" />
           <ToolbarButtonClass text="class" />
         </Toolbar>
+
+        <Outer/>
       </header>
     </div>
   );
